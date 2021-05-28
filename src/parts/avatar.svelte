ffbff91f72b51fdd5dc5b8db4dfe59ce187ea273
lisez <mm4324@gmail.com>
2021-05-28 22:56:14 +0800

    [feat] Add photos

--- src/parts/avatar.svelte
+++ src/parts/avatar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   export let name: string = '';
+  export let image: string = '/images/user.svg';
 </script>
 
 <figure>
-  <div class="rounded-full">
-    <img src="/images/user.svg" alt="the avatar of {name}" />
+  <div class="avatar rounded-full">
+    <img src={image} alt="the avatar of {name}" />
   </div>
   <figcaption class="pt-5 text-left">
     <address>{name}</address>
@@ -12,8 +13,9 @@
 </figure>
 
 <style>
-  img {
+  div.avatar {
     width: 100px;
     height: 100px;
+    overflow: hidden;
   }
 </style>
